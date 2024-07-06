147c35be7434bacf09ee2cb3e0426212ed80c8cc
RichardRodri26 <richardmanuel811@gmail.com>
2024-07-05 23:20:16 -0500

    avances

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,7 +6,6 @@ import { Component } from '@angular/core';
   styleUrl: './app.component.css',
 })
 export class AppComponent {
-  public title: string = "Mi primera app angular"
-  public counter: number = 10
+  public title: string = 'Mi primera app angular';
 
 }
